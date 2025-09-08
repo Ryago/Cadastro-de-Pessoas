dfb1f519c7d4219e8149a7a88ddf130acadba31c
ryago <ryago@OZAWA>
2025-09-08 01:06:09 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAtu = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtIdade.Location = new System.Drawing.Point(125, 190);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(218, 20);
+            this.txtIdade.TabIndex = 1;
+            this.txtIdade.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(125, 239);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(218, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(53, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Idade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "E-mail:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(125, 143);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(218, 20);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.Text = " ";
+            this.txtNome.TextChanged += new System.EventHandler(this.lbNome_TextChanged);
+            // 
+            // btnCad
+            // 
+            this.btnCad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCad.Location = new System.Drawing.Point(192, 302);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(75, 23);
+            this.btnCad.TabIndex = 6;
+            this.btnCad.Text = "Cadastrar";
+            this.btnCad.UseVisualStyleBackColor = true;
+            this.btnCad.Click += new System.EventHandler(this.btnCad_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(411, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(336, 246);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnAtu
+            // 
+            this.btnAtu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtu.Location = new System.Drawing.Point(541, 302);
+            this.btnAtu.Name = "btnAtu";
+            this.btnAtu.Size = new System.Drawing.Size(75, 23);
+            this.btnAtu.TabIndex = 8;
+            this.btnAtu.Text = "Atualizar";
+            this.btnAtu.UseVisualStyleBackColor = true;
+            this.btnAtu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(55, 62);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(288, 25);
+            this.lblTitulo.TabIndex = 9;
+            this.lblTitulo.Text = "CADASTRO DE PESSOAS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnAtu);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Pessoas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAtu;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
+
---
+++ Teste/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtIdade.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Por favor utilize apenas números.");
+                txtIdade.Text = txtIdade.Text.Remove(txtIdade.Text.Length - 1);
+            }
+        }
+
+        private void lbNome_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCad_Click(object sender, EventArgs e)
+        {
+            var StrConexao = "server=localhost;uid=root;pwd=password;database=teste";
+            var conexao = new MySqlConnection(StrConexao);
+            conexao.Open();
+
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtIdade.Text) || string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Todos os campos devem estar preenchidos corretamente.");
+            }
+            else 
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand("INSERT INTO pessoa VALUES (null, '" + txtNome.Text + "', " + txtIdade.Text + ", '" + txtEmail.Text + "')", conexao);
+                    cmd.ExecuteReader();
+
+                    MessageBox.Show("Pessoa " + txtNome.Text + " cadastrado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro no cadastro de pessoa: " + ex.Message);
+                }
+            } 
+            conexao.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = load();
+        }
+        private BindingSource load()
+        {
+            var StrConexao = "server=localhost;uid=root;pwd=password;database=teste";
+            var conexao = new MySqlConnection(StrConexao);
+            conexao.Open();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand cmd;
+            DataSet ds = new DataSet();
+            BindingSource bs = new BindingSource();
+
+            string sql = "SELECT * FROM pessoa";
+            cmd = new MySqlCommand(sql, conexao);
+
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds);
+
+            bs.DataSource = ds.Tables[0];
+
+            return bs;
+        }
+    }
+}
